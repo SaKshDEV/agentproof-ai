48819019b19cc94a4e5b8a60cf1b0f209ee167b0
saksham000003 <2k23csaiml2314139@gmail.com>
2026-09-06 22:56:23 +0530

    authcontroller and authRoutes added

---
+++ server/controllers/authController.js
@@ -0,0 +1,52 @@
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "Please provide all required fields",
+      });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User with this email already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      message: "Account created successfully",
+
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Register error:", error.message);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"
 import ConnectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 ConnectDB();
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 app.get("/", (req,res)=>{
     res.send("Agentproof api is running");
 
