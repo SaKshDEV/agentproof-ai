8c668010b8e518e1455afdcdb1620f2b3beaecc5
saksham000003 <2k23csaiml2314139@gmail.com>
2026-09-07 12:39:38 +0530

    loign api and jwt done

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,14 @@
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+  return jwt.sign(
+    { userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+};
 
 export const registerUser = async (req, res) => {
   try {
@@ -44,6 +53,60 @@ export const registerUser = async (req, res) => {
     });
   } catch (error) {
     console.error("Register error:", error.message);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const normalizedEmail = email.trim().toLowerCase();
+
+    const user = await User.findOne({
+      email: normalizedEmail,
+    });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password"
+      });
+    }
+
+    const passwordMatches = await bcrypt.compare(
+      password,
+      user.password
+    );
+    if (!passwordMatches) {
+      return res.status(401).json({
+        message: "Invalid email or password"
+      });
+    }
+
+    const token = generateToken(user._id);
+
+    return res.status(200).json({
+      message: "Login successful",
+
+      token,
+
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+
+      },
+    });
+  } catch (error) {
+    console.error("Login error:", error.message);
 
     return res.status(500).json({
       message: "Server error",
