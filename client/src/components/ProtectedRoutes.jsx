fd3f6eb468819209ec72ad468965f61b2f981fec
saksham000003 <2k23csaiml2314139@gmail.com>
2026-09-08 14:14:32 +0530

    single token change to backend verification

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -1,9 +1,53 @@
 import { Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import api from "../services/api";
 
 function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
 
-  if (!token) {
+  useEffect(() => {
+    const verifyUser = async () => {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        setIsAuthenticated(false);
+        return;
+      }
+      try {
+        const response = await api.get("/auth/me", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        localStorage.setItem(
+          "user",
+          JSON.stringify(response.data.user)
+        );
+
+        setIsAuthenticated(true);
+
+      } catch (error) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+
+        setIsAuthenticated(false);
+      }
+    };
+
+    verifyUser();
+  }, []);
+
+  if (isAuthenticated === null) {
+    return (
+      <div className="flex min-h-screen items-center justify-center bg-slate-950 text-slate-400">
+        Checking authentication...
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
 
