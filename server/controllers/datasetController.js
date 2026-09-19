06e858af6ccc03e0bf8a070efb5f104d8dbcb0c5
saksham000003 <2k23csaiml2314139@gmail.com>
2026-09-19 23:21:51 +0530

    updated teh dataset controller

--- server/controllers/datasetController.js
+++ server/controllers/datasetController.js
@@ -62,3 +62,80 @@ export const getDatasets = async (req, res) => {
     });
   }
 };
+export const getDatasetById = async (req, res) => {
+  try {
+    const dataset = await Dataset.findOne({
+      _id: req.params.id,
+      user: req.user._id,
+    });
+
+    if (!dataset) {
+      return res.status(404).json({
+        message: "Dataset not found",
+      });
+    }
+
+    return res.status(200).json({
+      dataset,
+    });
+
+  } catch (error) {
+    console.error(
+      "Get dataset error:",
+      error.message
+    );
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+export const updateDataset = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      testCases,
+    } = req.body;
+
+    const dataset = await Dataset.findOne({
+      _id: req.params.id,
+      user: req.user._id,
+    });
+
+    if (!dataset) {
+      return res.status(404).json({
+        message: "Dataset not found",
+      });
+    }
+
+    if (name !== undefined) {
+      dataset.name = name;
+    }
+
+    if (description !== undefined) {
+      dataset.description = description;
+    }
+
+    if (testCases !== undefined) {
+      dataset.testCases = testCases;
+    }
+
+    await dataset.save();
+
+    return res.status(200).json({
+      message: "Dataset updated successfully",
+      dataset,
+    });
+
+  } catch (error) {
+    console.error(
+      "Update dataset error:",
+      error.message
+    );
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
